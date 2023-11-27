5c2a911789d0e3da9e62b6af8328d75294693f53
LeenaA <65114004+LeenaA-cloud@users.noreply.github.com>
2023-11-27 14:34:21 +0200

    Add files via upload

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,24 +1,51 @@
 import codecs
 
 def encode(s):
-    if not isinstance(s,str):
-        raise TypeError
-    origlen = len(s)
-    crypted = ""
-    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    if not isinstance(s, str):
+        raise TypeError("Input must be a string")
+    
     if len(s) > 1000:
-        raise ValueError
+        raise ValueError("Input length must be at most 1000 characters")
+    
+    crypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=', '!"#€%&/()=1234567890'))
+    
+    # Check for specific characters to avoid
+    for c in s:
+        if c.lower() in ["+", "å", "ä", "ö"]:
+            raise ValueError("Invalid character found: +, å, ä, or ö")
+    
+    # Encode the string
     for c in s:
         if c.isalpha():
             if c.islower():
-                c=c.upper()
-            # Rot13 the character for maximum security
-            crypted+=codecs.encode(c,'rot13')
+                c = c.upper()
+                crypted += codecs.encode(c, 'rot_13')
+            elif c.isupper():
+                c = c.lower()
+                crypted += codecs.encode(c, 'rot_13')
         elif c in digitmapping:
-          crypted+=digitmapping[c]
-
+            crypted += digitmapping[c]
+        # Handle other characters here if needed
+    
     return crypted
 
-def decode(s):
-    return s
+import codecs
 
+def decode(s):
+    decrypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=', '!"#€%&/()=1234567890'))
+    
+    for c in s:
+        if c.isalpha():
+            if c.isupper():
+                c = c.lower()
+            # Reverse ROT13 encryption
+            decrypted += codecs.decode(c, 'rot_13')
+        elif c in digitmapping.values():
+            # Reverse the digit mapping
+            for key, value in digitmapping.items():
+                if value == c:
+                    decrypted += key
+    
+    return decrypted
